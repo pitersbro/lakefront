19ed8f8a540dd5921292817cb0ed149807713db0
piotrnowakowski-co <piotr.nowakowski@cdq.com>
2026-04-27 23:23:06 +0200

    fix: toml properties not being overriden by corresponding env vars
    Closes #15

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -1,3 +1,5 @@
+import operator
+
 import pytest
 
 from lakefront import core
@@ -45,3 +47,38 @@ def test_create_raises_if_exists(tmp_path, monkeypatch):
     svc.create("dupe")
     with pytest.raises(core.ProjectExistsError):
         svc.create("dupe")
+
+
+@pytest.mark.parametrize(
+    "env_var,attr_path,value",
+    [
+        pytest.param("LAKEFRONT_CORE__THEME", "core.theme", "dracula", id="core.theme"),
+        pytest.param(
+            "LAKEFRONT_CORE__ANALYZER_ROW_LIMIT",
+            "core.analyzer_row_limit",
+            10,
+            id="core.analyzer_row_limit",
+        ),
+        pytest.param(
+            "LAKEFRONT_S3__ENDPOINT",
+            "s3.endpoint",
+            "https://s3.example.com",
+            id="s3.endpoint",
+        ),
+    ],
+)
+def test_settings_toml_properties_overriden_by_corresponsing_env_vars(
+    env_var, attr_path, value, monkeypatch
+):
+    monkeypatch.setenv(env_var, value)
+
+    settings = core.load_settings("testing")
+    assert operator.attrgetter(attr_path)(settings) == value
+
+
+def test_settings_do_pick_up_unsupported_env_var(monkeypatch):
+    monkeypatch.setenv("LAKEFRONT_CUSTOM__VALUE", "custom123")
+
+    settings = core.load_settings("testing")
+    with pytest.raises(AttributeError):
+        settings.custom.value == "custom123"  # type: ignore
